e118c851287d2053faf1f9938178fbd515d2f734
enhot <enhotkh@gmail.com>
2024-05-25 13:27:26 +0300

    page-addProject

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatBadgeModule, MatButtonModule, MatIconModule],
+  imports: [MatBadgeModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
