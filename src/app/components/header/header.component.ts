52b8a3a775875972017637e66f95268d90aab910
enhot <enhotkh@gmail.com>
2024-07-28 12:40:11 +0300

    dialog serv

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,10 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
 import { RouterLink } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { SendProjectFormService } from '../../services/send-project-form.service';
-import { map } from 'rxjs';
-import { Dialog, DialogModule } from '@angular/cdk/dialog';
-import { DialogHeaderComponent } from './dialog/dialog-header/dialog-header.component';
+import { DialogService } from '../../services/dialog.service';
 
 @Component({
   selector: 'app-header',
@@ -18,8 +15,6 @@ import { DialogHeaderComponent } from './dialog/dialog-header/dialog-header.comp
     MatIconModule,
     RouterLink,
     HttpClientModule,
-    DialogModule,
-    DialogHeaderComponent,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
@@ -27,45 +22,16 @@ import { DialogHeaderComponent } from './dialog/dialog-header/dialog-header.comp
 export class HeaderComponent implements OnInit {
   public hidden = false;
   public countNotification: number = 0;
-  public allDateEnd: Date[] = [];
-  constructor(
-    private getProjectFormService: SendProjectFormService,
-    public dialog: Dialog
-  ) {}
-  public ngOnInit(): void {
-    this.countDateEnd();
-  }
 
-  openDialog() {
-    this.dialog.open(DialogHeaderComponent, {
-      width: '80%',
-      height: 'auto',
-      data: {
-        date: this.allDateEnd,
-      },
+  constructor(private dialogService: DialogService) {}
+  public ngOnInit(): void {
+    this.dialogService.countNotification.subscribe((e) => {
+      this.countNotification = e;
     });
   }
 
-  public countDateEnd(): void {
-    this.getProjectFormService
-      .getTaskForm()
-      .pipe(
-        map((dates) => {
-          this.allDateEnd = []; // Очищаем массив перед добавлением новых данных
-          dates.forEach((e) => {
-            let nowDate = new Date();
-            let lastDate = new Date(e.dateEnd);
-            // добавляем только даты более рание или нынешнии
-            if (nowDate >= lastDate) {
-              this.allDateEnd.push(lastDate);
-            }
-          });
-
-          this.countNotification = this.allDateEnd.length;
-          console.log('my date', this.allDateEnd);
-        })
-      )
-      .subscribe();
+  public openDialog() {
+    this.dialogService.openDialog();
   }
   public toggleBadgeVisibility() {
     this.hidden = true;
---
+++ src/app/services/dialog.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Dialog, DialogModule } from '@angular/cdk/dialog';
+import { DialogHeaderComponent } from '../components/header/dialog/dialog-header/dialog-header.component';
+import { SendProjectFormService } from './send-project-form.service';
+import { BehaviorSubject, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DialogService {
+  public allDateEnd: Date[] = [];
+  public countNotification: BehaviorSubject<number> =
+    new BehaviorSubject<number>(0);
+
+  constructor(
+    public dialog: Dialog,
+    private getProjectFormService: SendProjectFormService
+  ) {
+    this.countDateEnd();
+  }
+  openDialog(): void {
+    this.dialog.open(DialogHeaderComponent, {
+      width: '80%',
+      height: 'auto',
+      data: {
+        date: this.allDateEnd,
+      },
+    });
+  }
+  public closeDialog(): void {}
+
+  public countDateEnd(): void {
+    this.getProjectFormService
+      .getTaskForm()
+      .pipe(
+        map((dates) => {
+          this.allDateEnd = []; // Очищаем массив перед добавлением новых данных
+          dates.forEach((e) => {
+            let nowDate = new Date();
+            let lastDate = new Date(e.dateEnd);
+            // добавляем только даты более рание или нынешнии
+            if (nowDate >= lastDate) {
+              this.allDateEnd.push(lastDate);
+            }
+          });
+
+          this.countNotification.next(this.allDateEnd.length);
+          console.log('my date', this.allDateEnd);
+        })
+      )
+      .subscribe();
+  }
+}
